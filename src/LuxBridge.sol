30b84a4af4ecfca4f73d9e13baa9a50c992a88a6
Zach Kelling <z@zeekay.io>
2022-10-30 19:57:23 -0400

    Rename contract

--- src/LuxBridge.sol
+++ src/LuxBridge.sol
@@ -1042,7 +1042,7 @@ contract ERC20WrappedAsset is ERC20, Ownable, AccessControl {
         emit AdminGranted(to);
     }
 
-    function revokeAdmin(address to) public onlyAdmin { 
+    function revokeAdmin(address to) public onlyAdmin {
         require(hasRole(DEFAULT_ADMIN_ROLE, to), "Ownable");
         revokeRole(DEFAULT_ADMIN_ROLE, to);
         emit AdminRevoked(to);
@@ -1291,14 +1291,14 @@ library SafeMath {
     }
 }
 
-// File: contracts/MultiTeleportBridge.sol
+// File: contracts/LuxBridge.sol
 
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 
-contract MultiTeleportBridge is Ownable, AccessControl {
- 
+contract LuxBridge is Ownable, AccessControl {
+
     using SafeMath for uint256;
     uint256 internal fee = 0; //zero default
     uint256 public feeRate = 10 * (uint256(10) ** 15); // Fee rate 1%
@@ -1312,14 +1312,14 @@ contract MultiTeleportBridge is Ownable, AccessControl {
     event BridgeMinted(address recipient, address token, uint256 amt);
     event AdminGranted(address to);
     event AdminRevoked(address to);
-    
+
 
     constructor() {
         _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
     }
 
-    /* 
-     * dev Sets admins 
+    /*
+     * dev Sets admins
      */
     modifier onlyAdmin() {
         require(hasRole(DEFAULT_ADMIN_ROLE, msg.sender), "Ownable");
@@ -1331,14 +1331,14 @@ contract MultiTeleportBridge is Ownable, AccessControl {
         emit AdminGranted(to);
     }
 
-    function revokeAdmin(address to) public onlyAdmin { 
+    function revokeAdmin(address to) public onlyAdmin {
         require(hasRole(DEFAULT_ADMIN_ROLE, to), "Ownable");
         revokeRole(DEFAULT_ADMIN_ROLE, to);
         emit AdminRevoked(to);
     }
 
     /*
-     * dev Set fee payout addresses and fee - set at contract launch - in wei 
+     * dev Set fee payout addresses and fee - set at contract launch - in wei
      */
     function setPayoutAddress(address addr, uint256 feeR) public onlyAdmin{
         payoutAddr = addr;
@@ -1346,23 +1346,23 @@ contract MultiTeleportBridge is Ownable, AccessControl {
     }
 
     /*
-     * dev: Mappings 
+     * dev: Mappings
      */
     struct MPCOracleAddrInfo{
         bool exists;
     }
-    
-    /*
-     * dev: Map MPCOracle address at blockHeight 
-     */
-    mapping(address => MPCOracleAddrInfo) internal MPCOracleAddrMap; 
 
-    function addMPCMapping(address _key) internal { 
+    /*
+     * dev: Map MPCOracle address at blockHeight
+     */
+    mapping(address => MPCOracleAddrInfo) internal MPCOracleAddrMap;
+
+    function addMPCMapping(address _key) internal {
         MPCOracleAddrMap[_key].exists = true;
     }
 
-    /* 
-     * dev: Used to set a new MPC address at block height - only MPC signers can update 
+    /*
+     * dev: Used to set a new MPC address at block height - only MPC signers can update
      */
     function setMPCOracle (address MPCO) public onlyAdmin {
         addMPCMapping(MPCO); // store in mapping.
@@ -1370,19 +1370,19 @@ contract MultiTeleportBridge is Ownable, AccessControl {
     }
 
     function getMPCMapDataTx(address _key) public view returns (bool){
-        return MPCOracleAddrMap[_key].exists; 
+        return MPCOracleAddrMap[_key].exists;
     }
 
-    /* 
-     * dev: Struct for mapping transaction history 
+    /*
+     * dev: Struct for mapping transaction history
      */
     struct TransactionInfo{
-        string txid; 
+        string txid;
         bool exists;
         //bool isStealth;
     }
-    
-    mapping(bytes => TransactionInfo) internal transactionMap; 
+
+    mapping(bytes => TransactionInfo) internal transactionMap;
 
     function addMappingStealth(bytes memory _key) internal {
         require(!transactionMap[_key].exists);
@@ -1391,29 +1391,29 @@ contract MultiTeleportBridge is Ownable, AccessControl {
     }
 
     function keyExistsTx(bytes memory _key) public view returns (bool){
-        return transactionMap[_key].exists; 
+        return transactionMap[_key].exists;
     }
 
 
     /*
-     * dev: Burns the msg.senders coins 
+     * dev: Burns the msg.senders coins
      */
-    function bridgeBurn(uint256 amount, address tokenAddr) public { 
+    function bridgeBurn(uint256 amount, address tokenAddr) public {
         VarStruct memory varStruct;
         varStruct.token = ERC20WrappedAsset(tokenAddr);
         require((varStruct.token.balanceOf(msg.sender) > 0), "ZeroBal");
-        varStruct.token.burnIt(msg.sender, amount); 
+        varStruct.token.burnIt(msg.sender, amount);
         emit BridgeBurned(msg.sender, amount);
     }
 
-    /* 
-     * dev Concat data to sign 
+    /*
+     * dev Concat data to sign
      */
     function append(string memory amt, string memory toTargetAddrStr, string memory txid, string memory tokenAddrStrHash, string memory chainIdStr, string memory vault) internal pure returns (string memory) {
         return string(abi.encodePacked(amt, toTargetAddrStr, txid, tokenAddrStrHash, chainIdStr, vault));
     }
 
-  
+
 
     struct VarStruct {
         bytes32 tokenAddrHash;
@@ -1426,8 +1426,8 @@ contract MultiTeleportBridge is Ownable, AccessControl {
 
 
     /*
-     * dev Sig is specific to recipient target address, hashed txid, amount, block height, target token and target chain  
-     * Sig can only be claimed once.  
+     * dev Sig is specific to recipient target address, hashed txid, amount, block height, target token and target chain
+     * Sig can only be claimed once.
      */
     function bridgeMintStealth(uint256 amt, string memory hashedId, address toTargetAddrStr, bytes memory signedTXInfo, address tokenAddrStr, string memory chainId, string memory vault) public returns (address) {
 
@@ -1435,16 +1435,16 @@ contract MultiTeleportBridge is Ownable, AccessControl {
 
         varStruct.tokenAddrHash = keccak256(abi.encodePacked(tokenAddrStr));
         varStruct.token = ERC20WrappedAsset(tokenAddrStr);
-        varStruct.toTargetAddr = toTargetAddrStr; 
+        varStruct.toTargetAddr = toTargetAddrStr;
         varStruct.toTargetAddrStrHash = keccak256(abi.encodePacked(toTargetAddrStr));
         varStruct.amtStr = Strings.toString(amt);
         varStruct.toChainIdHash = keccak256(abi.encodePacked(chainId));
-    
+
         // Concat msg
         string memory msg1 = append(varStruct.amtStr, Strings.toHexString(uint256(varStruct.toTargetAddrStrHash), 32), hashedId, Strings.toHexString(uint256(varStruct.tokenAddrHash), 32), Strings.toHexString(uint256(varStruct.toChainIdHash), 32), vault);
 
         // Check signedTXInfo doesn't already exist
-        require(!transactionMap[signedTXInfo].exists, "DupeTX"); 
+        require(!transactionMap[signedTXInfo].exists, "DupeTX");
 
         address signer = recoverSigner(prefixed(keccak256(abi.encodePacked(msg1))), signedTXInfo);
 
@@ -1458,15 +1458,15 @@ contract MultiTeleportBridge is Ownable, AccessControl {
         varStruct.token.mint(payoutAddr, fee);
         varStruct.token.mint(varStruct.toTargetAddr, amt);
 
-        //add new txid Mapping 
+        //add new txid Mapping
         addMappingStealth(signedTXInfo);
-        
+
         emit BridgeMinted(varStruct.toTargetAddr, tokenAddrStr, amt);
-        
+
         return signer;
     }
 
-  
+
     function splitSignature(bytes memory sig) internal pure returns (uint8, bytes32, bytes32)
     {
         require(sig.length == 65);
@@ -1493,10 +1493,10 @@ contract MultiTeleportBridge is Ownable, AccessControl {
         bytes32 s;
         (v, r, s) = splitSignature(sig);
         return ecrecover(message, v, r, s);
-    } 
+    }
 
-    /* 
-     * dev: Builds a prefixed hash to mimic the behavior of eth_sign. 
+    /*
+     * dev: Builds a prefixed hash to mimic the behavior of eth_sign.
      */
     function prefixed(bytes32 hash) internal pure returns (bytes32) {
         return keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", hash));
