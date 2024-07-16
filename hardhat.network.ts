946f08ed64fffd1cd634842bf63559dacdea31cb
cco <cco@lingqingmeng.com>
2024-07-16 14:59:30 +0800

    no more goerli

--- hardhat.network.ts
+++ hardhat.network.ts
@@ -55,7 +55,7 @@ const networks: HardhatUserConfig['networks'] = {
     },
   },
   testnet: {
-    url: `https://eth-goerli.g.alchemy.com/v2/${process.env.GOERLI_ALCHEMY_ID}`,
+    url: `https://eth-sepolia.g.alchemy.com/v2/${process.env.GOERLI_ALCHEMY_ID}`,
     chainId: 5,
     gasPrice: 10e9,
     gas: 10e6,
