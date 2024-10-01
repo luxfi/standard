873f0cbffd356cc26040ee507249959b789d2429
foxier25 <danibaker0388@gmail.com>
2024-10-01 14:24:36 -0500

    add auto-switch networks and deployments.

--- deploy/20_uniswapv3_core.ts
+++ deploy/20_uniswapv3_core.ts
@@ -1,36 +1,42 @@
 import hre, { ethers } from "hardhat";
-import {SignerWithAddress} from "@nomiclabs/hardhat-ethers/signers";
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 const fs = require('fs');  // Import the File System module
 
-//(mainnet) npx hardhat run --network lux deploy/20_uniswapv3_core.ts
-//(testnet) npx hardhat run --network lux_testnet deploy/20_uniswapv3_core.ts
+// (mainnet) npx hardhat run --network lux deploy/20_uniswapv3_core.ts
+// (testnet) npx hardhat run --network lux_testnet deploy/20_uniswapv3_core.ts
+
 async function main() {
     let deployer: SignerWithAddress;
-
     [deployer] = await ethers.getSigners();
 
+    // Deploy UniswapV3Factory contract
     const univ3FactoryFactory = await ethers.getContractFactory("UniswapV3Factory", deployer);
-
     const factoryv3 = await univ3FactoryFactory.deploy();
-
     await factoryv3.deployed();
     console.log(`Factory v3 deployed at: ${factoryv3.address}`);
 
+    // Verify the contract on Etherscan
     await hre.run("verify:verify", {
         address: factoryv3.address,
         contract: "src/uni3/core/contracts/UniswapV3Factory.sol:UniswapV3Factory",
-      constructorArguments: [],
+        constructorArguments: [],
     });
 
-     // Load the ABI from the compiled contract artifacts
+    // Determine the folder based on the network
+    const networkName = hre.network.name; // "lux" or "lux_testnet"
+    const folder = networkName === "lux" ? "mainnet" : "testnet";
+
+    // Load the ABI from the compiled contract artifacts
     const abi = JSON.parse(fs.readFileSync('./artifacts/src/uni3/core/contracts/UniswapV3Factory.sol/UniswapV3Factory.json', 'utf8')).abi;
 
-    fs.writeFileSync('deployments/mainnet/UniswapV3Factory.json', JSON.stringify({
+    // Write ABI and address to the corresponding folder
+    const outputPath = `deployments/${folder}/UniswapV3Factory.json`;
+    fs.writeFileSync(outputPath, JSON.stringify({
         address: factoryv3.address,
         abi: abi,
     }, null, 2));
 
-    console.log('Contract ABI and address saved to UniswapV3Factory.json');
+    console.log(`Contract ABI and address saved to ${outputPath}`);
     return;
 }
 
