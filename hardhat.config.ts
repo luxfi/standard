e5c91c4e96f5ada0d6c43b4c078ac2db704350ba
foxier25 <danibaker0388@gmail.com>
2024-09-30 20:57:57 -0500

    add abis and hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,81 +1,121 @@
-import { HardhatUserConfig } from 'hardhat/types'
-import 'hardhat-deploy'
-import '@typechain/hardhat'
-import '@nomiclabs/hardhat-web3'
 import '@nomiclabs/hardhat-ethers'
+import '@nomiclabs/hardhat-etherscan'
 import '@nomiclabs/hardhat-waffle'
-import "@nomiclabs/hardhat-etherscan"
-import '@openzeppelin/hardhat-upgrades'
-import 'ethers'
+import 'hardhat-typechain'
+import 'hardhat-watcher'
+import * as dotenv from "dotenv";
 
-require('dotenv').config()
+dotenv.config();
 
-import networks from './hardhat.network'
-
-const config: HardhatUserConfig = {
-  networks,
-
-  solidity: {
-    compilers: [
-      {
-        version: '0.4.24',
-        settings: {
-          optimizer: {
-            enabled: true,
-            runs: 200,
-          },
-        },
-      },
-      {
-        version: '0.8.4',
-        settings: {
-          optimizer: {
-            enabled: true,
-            runs: 20,
-          },
-        },
-      },
-    ],
-  },
-
-  namedAccounts: {
-    deployer: {
-      default: 0, // here this will by default take the first account as deployer
+const LOW_OPTIMIZER_COMPILER_SETTINGS = {
+  version: '0.7.6',
+  settings: {
+    evmVersion: 'istanbul',
+    optimizer: {
+      enabled: true,
+      runs: 2_000,
     },
-    dao: {
-      default: 1,
+    metadata: {
+      bytecodeHash: 'none',
     },
   },
-
-  paths: {
-    sources: './src',
-  },
-
-  typechain: {
-    outDir: './types',
-    target: 'ethers-v5',
-    alwaysGenerateOverloads: false,
-    externalArtifacts: [],
-  },
-
-  mocha: {
-    timeout: 20000000,
-    // parallel: true,
-  },
-
-  etherscan: {
-    apiKey: process.env.ETHERSCAN_API_KEY
-  }
 }
 
-task("accounts", "Prints the list of accounts", async () => {
-  const accounts = await ethers.getSigners();
+const LOWEST_OPTIMIZER_COMPILER_SETTINGS = {
+  version: '0.7.6',
+  settings: {
+    evmVersion: 'istanbul',
+    optimizer: {
+      enabled: true,
+      runs: 1_000,
+    },
+    metadata: {
+      bytecodeHash: 'none',
+    },
+  },
+}
 
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
+const DEFAULT_COMPILER_SETTINGS = {
+  version: '0.7.6',
+  settings: {
+    evmVersion: 'istanbul',
+    optimizer: {
+      enabled: true,
+      runs: 1_000_000,
+    },
+    metadata: {
+      bytecodeHash: 'none',
+    },
+  },
+}
 
-export {}
-
-export default config
+export default {
+  networks: {
+    hardhat: {
+      allowUnlimitedContractSize: false,
+    },
+    mainnet: {
+      url: `https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
+    },
+    ropsten: {
+      url: `https://ropsten.infura.io/v3/${process.env.INFURA_API_KEY}`,
+    },
+    rinkeby: {
+      url: `https://rinkeby.infura.io/v3/${process.env.INFURA_API_KEY}`,
+    },
+    goerli: {
+      url: `https://goerli.infura.io/v3/${process.env.INFURA_API_KEY}`,
+    },
+    kovan: {
+      url: `https://kovan.infura.io/v3/${process.env.INFURA_API_KEY}`,
+    },
+    arbitrumRinkeby: {
+      url: `https://arbitrum-rinkeby.infura.io/v3/${process.env.INFURA_API_KEY}`,
+    },
+    arbitrum: {
+      url: `https://arbitrum-mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
+    },
+    optimismKovan: {
+      url: `https://optimism-kovan.infura.io/v3/${process.env.INFURA_API_KEY}`,
+    },
+    optimism: {
+      url: `https://optimism-mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
+    },
+    lux: {
+      url: "https://api.lux.network/",
+      accounts: [process.env.PK ?? ""],
+    },
+    lux_testnet: {
+      url: "https://api.lux-test.network",
+      accounts: [process.env.PK ?? ""],
+    }
+  },
+  etherscan: {
+    // Your API key for Etherscan
+    // Obtain one at https://etherscan.io/
+    apiKey: {
+      sepolia: process.env.ETHERSCAN_API_KEY!,
+      bsc: process.env.BSCSCAN_API_KEY!,
+      bscTestnet: process.env.BSCSCAN_API_KEY!,
+      lux: "", // Leave empty if not applicable
+      "lux-test": "" // Leave empty if not applicable
+    },
+  },
+  solidity: {
+    compilers: [DEFAULT_COMPILER_SETTINGS],
+    overrides: {
+      'contracts/NonfungiblePositionManager.sol': LOW_OPTIMIZER_COMPILER_SETTINGS,
+      'contracts/test/MockTimeNonfungiblePositionManager.sol': LOW_OPTIMIZER_COMPILER_SETTINGS,
+      'contracts/test/NFTDescriptorTest.sol': LOWEST_OPTIMIZER_COMPILER_SETTINGS,
+      'contracts/NonfungibleTokenPositionDescriptor.sol': LOWEST_OPTIMIZER_COMPILER_SETTINGS,
+      'contracts/libraries/NFTDescriptor.sol': LOWEST_OPTIMIZER_COMPILER_SETTINGS,
+    },
+  },
+  watcher: {
+    test: {
+      tasks: [{ command: 'test', params: { testFiles: ['{path}'] } }],
+      files: ['./test/**/*'],
+      verbose: true,
+    },
+  },
+}
