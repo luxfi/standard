10037b8b83ce8436d790f2c00a905ed2f8da6629
foxier25 <danibaker0388@gmail.com>
2024-10-01 14:24:36 -0500

    add auto-switch networks and deployments.

--- deploy/21_uniswapv3_periphery.ts
+++ deploy/21_uniswapv3_periphery.ts
@@ -4,22 +4,22 @@ const fs = require('fs');  // Import the File System module
 
 // (mainnet)  npx hardhat run --network lux deploy/21_uniswapv3_periphery.ts
 // (testnet)  npx hardhat run --network lux_testnet deploy/21_uniswapv3_periphery.ts
-// Before start, you should update factory and weth address as same net.
+
 async function main() {
     let deployer: SignerWithAddress;
 
     [deployer] = await ethers.getSigners();
 
-    // factory address (mainnet) 0x0650683db720c793ff7e609A08b5fc2792c91f39
-    // factory address (testnet) 0x036A0AE1D760D7582254059BeBD9e5A34062dE23
-    const factory = "0x036A0AE1D760D7582254059BeBD9e5A34062dE23";
+    // Determine the folder based on the network
+    const networkName = hre.network.name; // "lux" or "lux_testnet"
+    const folder = networkName === "lux" ? "mainnet" : "testnet";
+    const factory = networkName === "lux" ? "0x0650683db720c793ff7e609A08b5fc2792c91f39" : "0x036A0AE1D760D7582254059BeBD9e5A34062dE23";
+    const weth = networkName === "lux" ? "0x53B1aAA5b6DDFD4eD00D0A7b5Ef333dc74B605b5" : "0x0650683db720c793ff7e609A08b5fc2792c91f39";
 
-    // mock weth address (mainnet) 0x53B1aAA5b6DDFD4eD00D0A7b5Ef333dc74B605b5
-    // mock weth address (testnet) 0x0650683db720c793ff7e609A08b5fc2792c91f39
-    const weth = "0x0650683db720c793ff7e609A08b5fc2792c91f39";
     {
         const SwapRouter = await ethers.getContractFactory("SwapRouter");
         const swapRouter = await SwapRouter.deploy(factory, weth);
+        
 
         await swapRouter.deployed();
         console.log("Swap router:", swapRouter.address);
@@ -30,7 +30,7 @@ async function main() {
         constructorArguments: [factory, weth],
         });
 
-        fs.writeFileSync('deployments/mainnet/SwapRouter.json', JSON.stringify({
+        fs.writeFileSync(`deployments/${folder}/SwapRouter.json`, JSON.stringify({
             address: swapRouter.address,
             abi: JSON.parse(fs.readFileSync('./artifacts/src/uni3/periphery/contracts/SwapRouter.sol/SwapRouter.json', 'utf8')).abi,
         }, null, 2));
@@ -50,7 +50,7 @@ async function main() {
         constructorArguments: [factory, weth],
         });
 
-        fs.writeFileSync('deployments/mainnet/Quoter.json', JSON.stringify({
+        fs.writeFileSync(`deployments/${folder}/Quoter.json`, JSON.stringify({
             address: quoter.address,
             abi: JSON.parse(fs.readFileSync('./artifacts/src/uni3/periphery/contracts/lens/Quoter.sol/Quoter.json', 'utf8')).abi,
         }, null, 2));
@@ -69,7 +69,7 @@ async function main() {
             contract: "src/uni3/periphery/contracts/lens/QuoterV2.sol:QuoterV2",
         constructorArguments: [factory, weth],
         });
-        fs.writeFileSync('deployments/mainnet/QuoterV2.json', JSON.stringify({
+        fs.writeFileSync(`deployments/${folder}/QuoterV2.json`, JSON.stringify({
             address: quoter2.address,
             abi: JSON.parse(fs.readFileSync('./artifacts/src/uni3/periphery/contracts/lens/QuoterV2.sol/QuoterV2.json', 'utf8')).abi,
         }, null, 2));
@@ -88,7 +88,7 @@ async function main() {
         contract: "src/uni3/periphery/contracts/libraries/NFTDescriptor.sol:NFTDescriptor",
     constructorArguments: [],
     });
-    fs.writeFileSync('deployments/mainnet/NFTDescriptor.json', JSON.stringify({
+    fs.writeFileSync(`deployments/${folder}/NFTDescriptor.json`, JSON.stringify({
         address: nftDescriptor.address,
         abi: JSON.parse(fs.readFileSync('./artifacts/src/uni3/periphery/contracts/libraries/NFTDescriptor.sol/NFTDescriptor.json', 'utf8')).abi,
     }, null, 2));
@@ -112,7 +112,7 @@ async function main() {
         contract: "src/uni3/periphery/contracts/NonfungibleTokenPositionDescriptor.sol:NonfungibleTokenPositionDescriptor",
     constructorArguments: [weth, luxBytes32],
     });
-    fs.writeFileSync('deployments/mainnet/NonfungibleTokenPositionDescriptor.json', JSON.stringify({
+    fs.writeFileSync(`deployments/${folder}/NonfungibleTokenPositionDescriptor.json`, JSON.stringify({
         address: tokenDescriptor.address,
         abi: JSON.parse(fs.readFileSync('./artifacts/src/uni3/periphery/contracts/NonfungibleTokenPositionDescriptor.sol/NonfungibleTokenPositionDescriptor.json', 'utf8')).abi,
     }, null, 2));
@@ -130,7 +130,7 @@ async function main() {
         contract: "src/uni3/periphery/contracts/NonfungiblePositionManager.sol:NonfungiblePositionManager",
       constructorArguments: [factory, weth, tokenDescriptor.address],
     });
-    fs.writeFileSync('deployments/mainnet/NonfungiblePositionManager.json', JSON.stringify({
+    fs.writeFileSync(`deployments/${folder}/NonfungiblePositionManager.json`, JSON.stringify({
         address: positonManager.address,
         abi: JSON.parse(fs.readFileSync('./artifacts/src/uni3/periphery/contracts/NonfungiblePositionManager.sol/NonfungiblePositionManager.json', 'utf8')).abi,
     }, null, 2));
