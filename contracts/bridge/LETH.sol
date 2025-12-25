38c41cd8ddc99b413af74831dfbf34fd7caabdbb
Zach Kelling <z@zeekay.ai>
2025-12-25 13:11:32 -0800

    refactor: update synthetic token naming (s*) and use LRC standards
    - Change synthetic token prefix from x* to s* (sUSD, sETH, sBTC...)
    - Remove "DeFi -" prefix from README section headers
    - Update bridge tokens to use LRC20/LRC20B base classes
    - Delete duplicate ERC20B.sol (replaced by LRC20B.sol)
    - Add Multicall2 and Ethereum testnet deployments
    - Update AlchemistV2 -> SynthVault naming in docs

--- contracts/bridge/LETH.sol
+++ contracts/bridge/LETH.sol
@@ -2,32 +2,14 @@
 // Copyright (c) 2025 Lux Industries Inc.
 pragma solidity ^0.8.31;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "./LRC20B.sol";
 
-/// @title LETH - Bridged Ether on Lux Network
-/// @notice ERC20 representation of ETH bridged to Lux
-contract LETH is ERC20 {
-    event Deposit(address indexed dst, uint256 wad);
-    event Withdrawal(address indexed src, uint256 wad);
-
-    constructor() ERC20("Lux Ether", "LETH") {
-        // Pre-mint for testing/deployment
-        _mint(msg.sender, 10000000 * 10 ** decimals());
-    }
-
-    receive() external payable {
-        deposit();
-    }
-
-    function deposit() public payable {
-        _mint(msg.sender, msg.value);
-        emit Deposit(msg.sender, msg.value);
-    }
-
-    function withdraw(uint256 wad) public {
-        require(balanceOf(msg.sender) >= wad, "LETH: insufficient balance");
-        _burn(msg.sender, wad);
-        payable(msg.sender).transfer(wad);
-        emit Withdrawal(msg.sender, wad);
-    }
+/**
+ * @title LETH - Bridged Ether on Lux Network
+ * @notice LRC20B representation of ETH bridged to Lux via Teleport
+ * @dev Built on LRC20B (Lux Request for Comments 20 Bridgeable)
+ *      Supports admin-controlled bridgeMint/bridgeBurn for bridge operations
+ */
+contract LETH is LRC20B {
+    constructor() LRC20B("Lux Ether", "LETH") {}
 }
